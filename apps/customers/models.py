b6084b34a206677cb422ab05aaabdc562d2fa493
Ayush Karn <231210032@nitdelhi.ac.in>
2026-03-04 00:59:36 +0530

    phase-2: implement customer domain model

---
+++ apps/customers/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone_number = models.BigIntegerField(unique=True)
+    monthly_salary = models.IntegerField()
+    approved_limit = models.IntegerField()
+    current_debt = models.FloatField(default=0)
+
+    class Meta:
+        app_label = 'customers'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
